fd1441061129aebb0d17319415c5755a6c8a9014
momo066 <sabupine@gmail.com>
2026-02-25 12:24:45 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/hkdlapp/MainActivity.kt
+++ app/src/main/java/com/example/hkdlapp/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.hkdlapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Map
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
